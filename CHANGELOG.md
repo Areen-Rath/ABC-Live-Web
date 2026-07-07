ad55067a0745988b4a83027a8822cae218abddda
Areen-Rath <areenrath0805@gmail.com>
2026-07-07 21:54:20 +0530

    ABC Live Web v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # v1.0.0
 
 * Initial commit
+
+# v1.0.1
+
+* Make the entire news card clickable for touchscreen
--- sections/News.tsx
+++ sections/News.tsx
@@ -9,24 +9,24 @@ import {
 export default function News({ newsList, source }: { newsList: object[], source: string }) {
     return newsList.map((news: any) => {
         return (
-            <Card key={`${source} ${newsList.indexOf(news)}`} className="group flex flex-col border rounded-lg bg-background overflow-hidden">
-                <CardHeader>
-                    <CardDescription>
-                        Source: {source}
-                    </CardDescription>
-                    <a href={news.link} target="_blank">
+            <a href={news.link} target="_blank">
+                <Card key={`${source} ${newsList.indexOf(news)}`} className="group flex flex-col border rounded-lg bg-background overflow-hidden">
+                    <CardHeader>
+                        <CardDescription>
+                            Source: {source}
+                        </CardDescription>
                         <CardTitle className="mt-2 mb-2 ml-0 mr-0 font-bold text-2xl group-hover:text-blue-800 group-hover:underline">
                             {news.title}
                         </CardTitle>
-                    </a>
-                </CardHeader>
-                <CardContent>
-                    {news.desc}
-                </CardContent>
-                <CardContent className="mt-auto font-bold text-teal-800">
-                    Read Full Story
-                </CardContent>
-            </Card>
+                    </CardHeader>
+                    <CardContent>
+                        {news.desc}
+                    </CardContent>
+                    <CardContent className="mt-auto font-bold text-teal-800">
+                        Read Full Story
+                    </CardContent>
+                </Card>
+            </a>
         );
     });
 }
